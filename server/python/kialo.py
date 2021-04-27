7162e88733d9bf36a1aa5a6018819838d72ccd9a
Youmna-H <youmna.hussein@gmail.com>
2021-04-27 23:29:04 +0100

    prepare

--- server/python/kialo.py
+++ server/python/kialo.py
@@ -8,6 +8,7 @@ from six.moves import cPickle
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 import re
+import faiss_index
 np.random.seed(123)
 
 def clean_up_data(path, output_path):
@@ -37,220 +38,176 @@ def clean_up_data(path, output_path):
 	fw = open(output_path, 'w')
 	fw.write(to_write)
 
-
-if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-pre", "--pre_path", dest="pre_path", type=str, required=False, default="", help="Need to set to python/ when running from the server") #default is false in case of loading a pretrained model, and just testing on test set
-	parser.add_argument("-d", "--dataset", dest="dataset", type=str, required=False, default="drugs", help="The path to the data, could be one file or a directory") #default is false in case of loading a pretrained model, and just testing on test set
-	# parser.add_argument("-p", "--data_path", dest="data_path", type=str, required=False, default="/Users/youmna/Documents/OUM2021/MoralMazeData/Money", help="The path to the data, could be one file or a directory") #default is false in case of loading a pretrained model, and just testing on test set
-	parser.add_argument("-m", "--sim_model", dest="sim_model", choices=['word2vec', 'glove', 'tfidf', 'sbert'], type=str, required=False, default="tfidf", help="The similarity model") #default is false in case of loading a pretrained model, and just testing on test set
-	parser.add_argument("-q", "--query", dest="query", type=str, required=True, default="", help="User's query") #default is false in case of loading a pretrained model, and just testing on test set
-	parser.add_argument("-n", "--num_responses", dest="num_responses", type=int, required=False, default=5, help="Number of similar responses to retrieve") #default is false in case of loading a pretrained model, and just testing on test set
-	parser.add_argument("-rp", "--responses_per_stance", dest="responses_per_stance", choices = [0,1], type=int, required=False, default=1, help="Set to 1 if you want the number of responses to be per stance") #default is false in case of loading a pretrained model, and just testing on test set
-	parser.add_argument("-rr", "--responses_to_response", dest="responses_to_response", choices = ["arg","arg_response"], type=str, required=False, default="arg", help="Set to arg_response if you want to return the responses to the retrieved most similar utterances") #default is false in case of loading a pretrained model, and just testing on test set
-
-	args = parser.parse_args()
-	data_path = []
-	vec_path = []
-	stances_path = []
-	start = False
-	topic = ""
-	vectors, most_similar,  most_similar_pro, most_similar_con = [], [], [], []
-	texts = []
-	texts_pro = []
-	texts_con = []
-	query_text =  args.query
-	id_to_claim = {}
-	responses_to_claims = {}
-	lowercase_to_uppercase = {}
-	texts_to_stances = {}
-	pre_path = args.pre_path
-	id_to_stances = {}
-	text_to_stance_lower = {}
-	# pre_path = ""
-	#uncomment to clean up the data and generate "_cleaned" file, clean up is sometimes required because some kialo files have errors (e.g., endline errors)
-	# clean_up_data(pre_path + "kialoData/should-a-license-be-required-in-order-to-have-a-child-procreate-2368.txt", pre_path + "kialoData/should-a-license-be-required-in-order-to-have-a-child-procreate-2368_cleaned.txt")
+def load(topic, model_name, sub_model_name="paraphrase-distilroberta-base-v1", is_indexed=False, **kwargs):
+	pre_path = ""#"python/"
 	config_path = pre_path + "kialo_config.json"
+	data_path_pro, vec_path_pro, data_path_con, vec_path_con, data_path_all, vec_path_all, vec_path_pro_original, vec_path_con_original = "", "", "", "", "", "", "", ""
+	lowercase_to_uppercase = {}
+	texts_all, texts_pro, texts_con = [], [], []
+	stances = {}
+	responses = {}
 	with open(config_path) as f:
 		config = json.load(f)
 		for c in config["topics"]:
-			if args.dataset == c["id"]:
-				datasets = c["datasets"]
-				for d in datasets:
-					data_path.append(pre_path + d["data_path"])
-					vec_path.append(pre_path  + d["sbert_path"])
-					stances_path.append(pre_path + d["stances_path"])
-	
-	#read stances
-	for i,s in enumerate(stances_path):
-		with open(s) as f:
-			for line in f:
-				parts = line.strip().split('\t')
-				id_to_stances[str(i)+"_"+parts[0]] = parts[1] #add file index before id
-	
-	if data_path == []:
-		print("Incorrect topic!!!!")
-
+			if topic == c["id"]:
+				data_path_pro = pre_path + c["data_path_pro"]
+				vec_path_pro = pre_path  + c["sbert_path_pro"]
+				data_path_con = pre_path + c["data_path_con"]
+				vec_path_con = pre_path  + c["sbert_path_con"]
+				data_path_all = pre_path + c["data_path_all"]
+				vec_path_all = pre_path  + c["sbert_path_all"]
+				vec_path_pro_original = pre_path  + c["sbert_path_pro_original"]
+				vec_path_con_original = pre_path  + c["sbert_path_con_original"]
 	#read data files
-	lens = []
-	for file_index, path in enumerate(data_path):
-		start = False
-		with open(path) as f:
-			for line in f:
-				if line.startswith("1. "):
-					start = True
-					topic = line.strip().split(" ",1)[1]
-					id_to_claim[str(file_index)+"_"+"1."] = topic.lower()
-					responses_to_claims[topic.lower()] = []
-					continue
-				if start and line.strip() != "":
-					parts = line.strip().split(" ",2)
-					if len(parts) < 3:
-						continue
-					text = parts[2].lower()
-					
-					# words = word_tokenize(text)
-					# lens.append(len(words))
-					if text.startswith("-> see 1"):
-						ref = str(file_index)+"_"+text.split()[-1]
-						text = id_to_claim[ref]
-					else:
-						lowercase_to_uppercase[text] = parts[2]
-					texts.append(text)
-					_id = str(file_index)+"_"+parts[0]
-					stance = id_to_stances[_id]
-					if stance == 'pro':
-						texts_pro.append(text)
-						text_to_stance_lower[text] = "pro"
-					else:
-						texts_con.append(text)
-						text_to_stance_lower[text] = "con"
-					
-					responses_to_claims[text] = []
-					id_to_claim[_id] = text
-					initial_claim_id  = ".".join(_id.split(".")[:-2]) + "."
-					initial_claim = id_to_claim[initial_claim_id]
-					responses_to_claims[initial_claim].append({"stance":parts[1].lower()[:-1],"text":text})
-	texts.append(query_text)
-	texts_pro.append(query_text)
-	texts_con.append(query_text)
-
-	# print(responses_to_claims)
-	# print("##########")
-	# print(np.max(lens))
-	# print(np.mean(lens))
-
-	if args.sim_model == 'sbert':
-		vectors, vectors_pro, vectors_con = {}, {}, {}
-		for bert_path in vec_path:
-			infile = open(bert_path,'rb')
-			vectors.update(cPickle.load(infile))
-			infile.close()
-		if query_text not in vectors:
-			vectors[query_text] = utils.get_sbert_vec(query_text)
-		if args.responses_per_stance == 0:
-			most_similar.extend(utils.most_sim_cos(vectors, query_text, args.num_responses))
+	with open(data_path_pro) as f:
+		for line in f:
+			parts = line.strip().split("\t",4)
+			text = parts[0]
+			lowercase_to_uppercase[text] = parts[2]
+			texts_pro.append(text)
+			if len(parts) >= 5:
+				responses[text] = parts[4].split('\t')
+	with open(data_path_con) as f:
+		for line in f:
+			parts = line.strip().split("\t",4)
+			text = parts[0]
+			lowercase_to_uppercase[text] = parts[2]
+			texts_con.append(text)
+			if len(parts) >= 5:
+				responses[text] = parts[4].split('\t')
+	#load model
+	pro_vecs, con_vecs = [], []
+	model = None
+	if model_name == 'sbert':
+		model = utils.get_sbert_model(sub_model_name)
+		if is_indexed == 1:
+			models_pro_con = [faiss_index.load_index(vec_path_pro), faiss_index.load_index(vec_path_con)]
 		else:
-			vectors_pro.update({ d: vectors[d] for d in texts_pro })
-			most_similar_pro.extend(utils.most_sim_cos(vectors_pro, query_text, args.num_responses))
-			vectors_con.update({ d: vectors[d] for d in texts_con })
-			most_similar_con.extend(utils.most_sim_cos(vectors_con, query_text, args.num_responses))
-	elif args.sim_model == 'word2vec':
+			infile_pro = open(vec_path_pro_original,'rb')
+			infile_con = open(vec_path_con_original,'rb')
+			pro_vecs = cPickle.load(infile_pro)
+			con_vecs = cPickle.load(infile_con)
+			model = utils.get_sbert_model(sub_model_name)
+	elif model_name == 'word2vec':
 		path = pre_path + "embeddings/GoogleNews-vectors-negative300.bin"
 		model = gensim.models.KeyedVectors.load_word2vec_format(path, binary=True)
-		vectors = utils.get_vectors(model, texts)
-		if args.responses_per_stance == 0:
-			most_similar = utils.most_sim_cos(vectors, query_text, args.num_responses)
-		else:
-			vectors_pro = { d: vectors[d] for d in texts_pro }
-			most_similar_pro = utils.most_sim_cos(vectors_pro, query_text, args.num_responses)
-			vectors_con = { d: vectors[d] for d in texts_con }
-			most_similar_con = utils.most_sim_cos(vectors_con, query_text, args.num_responses)
-	elif args.sim_model == 'glove':
+		pro_vecs, model = utils.get_vectors(model, texts_pro)
+		con_vecs, model = utils.get_vectors(model, texts_con)
+	elif model_name == 'glove':
 		path = pre_path + "embeddings/glove.840B.300dword2vec.txt"
-		# path = '/Users/youmna/Documents/Phd_year2/Coherence_acl/GC_wsj/glove.6B.100d.word2vec.txt'
 		model = utils.read_emb_text(path)
-		vectors = utils.get_vectors(model, texts)
-		if args.responses_per_stance == 0:
-			most_similar = utils.most_sim_cos(vectors, query_text, args.num_responses)
-		else:
-			vectors_pro = { d: vectors[d] for d in texts_pro }
-			most_similar_pro = utils.most_sim_cos(vectors_pro, query_text, args.num_responses)
-			vectors_con = { d: vectors[d] for d in texts_con }
-			most_similar_con = utils.most_sim_cos(vectors_con, query_text, args.num_responses)
-	elif args.sim_model == 'tfidf':
-		if args.responses_per_stance == 0:
-			most_similar = utils.get_tfidf_sim(texts, args.num_responses, query_text)
-		else:
-			most_similar_pro = utils.get_tfidf_sim(texts_pro, args.num_responses, query_text)
-			most_similar_con = utils.get_tfidf_sim(texts_con, args.num_responses, query_text)
+		pro_vecs, model = utils.get_vectors(model, texts_pro)
+		con_vecs, model = utils.get_vectors(model, texts_con)
+	#return values
+	#model is an object
+	#pro_vecs, con_vecs are arrays of vectors
+	#texts_pro, texts_con are arrays of texts
+	#responses and lowercase_to_uppercase is a dictionaries
+	return {"model": model, "pro_vecs":pro_vecs, "con_vecs":con_vecs, "texts_pro":texts_pro, "texts_con":texts_con, "responses":responses, "lowercase_to_uppercase":lowercase_to_uppercase}
 
-	most_similar_response = []
-	most_similar_response_pro = []
-	most_similar_response_con = []
-	if args.responses_to_response == "arg_response":
-		if args.responses_per_stance == 0:
-			for claim in most_similar:
-				if claim in responses_to_claims:
-					for response in responses_to_claims[claim]:
-						most_similar_response.append(response["text"])
-			most_similar = most_similar_response[-args.num_responses:]
-		else:
-			for claim in most_similar_pro:
-				if claim in responses_to_claims:
-					for response in responses_to_claims[claim]:
-						stance = text_to_stance_lower[response["text"]]
-						if stance == 'pro':
-							most_similar_response_pro.append(response["text"])
-						else:
-							most_similar_response_con.append(response["text"])
-			for claim in most_similar_con:
-				if claim in responses_to_claims:
-					for response in responses_to_claims[claim]:
-						stance = text_to_stance_lower[response["text"]]
-						if stance == 'pro':
-							most_similar_response_pro.append(response["text"])
-						else:
-							most_similar_response_con.append(response["text"])
-
-			most_similar_pro = most_similar_response_pro[-args.num_responses:]
-			most_similar_con = most_similar_response_con[-args.num_responses:]
-	
-	if args.responses_per_stance == 0:
-		most_similar.reverse()
-		stances = [text_to_stance_lower[i] for i in most_similar]
-		original_uppercase_responses = []
-		for r in most_similar:
-			original_uppercase_responses.append(lowercase_to_uppercase[r])
-	
-		most_similar = original_uppercase_responses
-		print("###///".join(most_similar) + "$!$!$" + "###///".join(stances))
+def get_suggested_responses(query, model_name, model, pro_vecs, con_vecs, texts_pro, texts_con, responses, lowercase_to_uppercase, num_responses=5, classify_responses=True, responses_to_response=False, is_indexed=False, **kwargs):
+	pre_path = ""#"python/"
+	# import time
+	# start = time.time()
+	config_path = pre_path + "kialo_config.json"
+	most_similar, stances = [], []
+	distances_pro, distances_con, most_similar_pro, most_similar_con = [], [], [], []
+	query_text = query.lower()
+	query_vec = []
+	if model_name == 'tfidf':
+		texts_pro.append(query_text)
+		texts_con.append(query_text)
+		distances_pro, most_similar_pro = utils.get_tfidf_sim(texts_pro, num_responses, query_text)
+		distances_con, most_similar_con = utils.get_tfidf_sim(texts_con, num_responses, query_text)
 	else:
-		original_uppercase_responses_pro = []
-		for r in most_similar_pro:
-			original_uppercase_responses_pro.append(lowercase_to_uppercase[r])
-		original_uppercase_responses_con = []
-		for r in most_similar_con:
-			original_uppercase_responses_con.append(lowercase_to_uppercase[r])
-		most_similar_pro = original_uppercase_responses_pro
-		most_similar_con = original_uppercase_responses_con
-		most_similar_pro.reverse()
-		most_similar_con.reverse()
-		
-		pro_str = "###///".join(most_similar_pro) if len(most_similar_pro) > 0 else ""
-		con_str = "###///".join(most_similar_con) if len(most_similar_con) > 0 else ""
-		print( pro_str + "$!$!$" + con_str)
+		if model_name == 'sbert':
+			query_vec = utils.get_sbert_vec(model, query_text)
+		else:
+			query_vec = utils.get_vector(model, query_text)
+		if model_name == 'sbert' and is_indexed:
+			query_vec /= np.linalg.norm(query_vec, keepdims=True)
+			distances_pro, most_similar_pro = faiss_index.search(pro_vecs, query_vec, texts_pro, num_responses)
+			distances_con, most_similar_con = faiss_index.search(con_vecs, query_vec, texts_con, num_responses)
+		else:
+			distances_pro, most_similar_pro = utils.most_sim_cos(pro_vecs, query_vec, num_responses)
+			distances_con, most_similar_con = utils.most_sim_cos(con_vecs, query_vec, num_responses)
+	stances = ["pro"] * len(most_similar_pro)
+	stances.extend(["con"] * len(most_similar_con))
+	most_similar = most_similar_pro
+	most_similar.extend(most_similar_con) 
+	distances = distances_pro
+	distances.extend(distances_con)
+	if not classify_responses:
+		distances, most_similar, stances = zip(*sorted(zip(distances, most_similar, stances), reverse=True))
+		most_similar = most_similar[:num_responses]
+		stances = stances[:num_responses]
 
-
-	# if len(most_similar) == 0:
-	# 	most_similar.append("No Matches found!")
+	if responses_to_response:
+		stances = []
+		most_similar_response = []
+		for claim in most_similar:
+			if claim in responses:
+				for response in responses[claim]:
+					r = response.lower()
+					most_similar_response.append(r)
+					if r in texts_pro:
+						stances.append('pro')
+					else:
+						stances.append('con')
+		if classify_responses:
+			most_similar = []
+			temp_stances = []
+			pro_count, con_count = 0, 0
+			for i, claim in enumerate(most_similar_response):
+				if stances[i] == 'pro' and pro_count < num_responses:
+					most_similar.append(claim)
+					pro_count += 1
+					temp_stances.append('pro')
+				elif stances[i] == 'con' and con_count < num_responses:
+					most_similar.append(claim)
+					con_count += 1
+					temp_stances.append('con')
+			stances = temp_stances
+		else:
+			most_similar = most_similar_response[:num_responses]
+			stances = stances[:num_responses]
 	
+	#format return values
+	return_array = []
+	for i, text in enumerate(most_similar):
+		return_array.append({"text": lowercase_to_uppercase[text], "stance": stances[i]})
+
+	#format of returned value
+	# {
+	# 	"suggested_responses": [
+	# 		{
+	# 			"text": text1,
+	# 			"stance": stance1
+	# 		},
+	# 		{
+	# 			"text": text2,
+	# 			"stance": stance2
+	# 		}
+	# 	]
+	# }
+	return json.dumps({"suggested_responses": return_array})
+
+	# done = time.time()
+	# print("time:" + str(done - start))
 	
 
-	# print(_id)
-	# print('most similar sentence to query:  ')
-	
+if __name__ == "__main__":
+	model_name = "sbert"
+	query = "Veganism saves animal lives."
+	is_indexed = False
+	num_responses = 3
+	classify_responses = True
+	responses_to_response = False
+	topic = "veganism"
 
-	
-
-
+	loaded_values = load(topic, model_name, is_indexed=is_indexed)
+	print("loaded")
+	x = get_suggested_responses(query, model_name, loaded_values['model'], loaded_values['pro_vecs'], loaded_values['con_vecs'],
+	loaded_values['texts_pro'], loaded_values['texts_con'], loaded_values['responses'], loaded_values['lowercase_to_uppercase'],
+	 num_responses=num_responses, classify_responses=classify_responses, responses_to_response=responses_to_response, is_indexed=is_indexed)
+	print(x) 
